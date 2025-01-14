f4a454d13b52adc682245a13475d2be13a422139
yossavorn <yossavorn@gmail.com>
2025-01-14 15:25:56 +0700

    Test

--- accounts/src/main/java/com/yossavorn/accounts/constants/AccountConstant.java
+++ accounts/src/main/java/com/yossavorn/accounts/constants/AccountConstant.java
@@ -5,7 +5,7 @@ public final class AccountConstant {
         // restrict instantiation
     }
 
-    public static final String  SAVINGS = "Savings";
+    public static final String  SAVINGS = "Savings 2";
     public static final String  ADDRESS = "123 Main Street, New York";
     public static final String  STATUS_201 = "201";
     public static final String  MESSAGE_201 = "Account created successfully";
